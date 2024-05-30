19527e1483670fc43ce61212ab4ea28ec5033e6d
Vedant3008 <shahh7107@gmail.com>
2024-05-30 17:02:28 +0530

    new

--- For.cpp
+++ For.cpp
@@ -3,7 +3,26 @@ using namespace std;
 
 int main(){
     int i;
-    for (i=1;i<=5;i=i+1){
-        cout<<"Vedant"<<endl;
+    // for (i=1;i<=5;i=i+1){
+    //     cout<<"Vedant"<<endl;
+    // } 
+
+    // for (i=1; i<=10; i=i+1){
+    //     cout<<i<<" ";
+    //     if (i==5){
+    //         break;
+    //     }
+    // }
+
+    // for (i=1; i<=5; i=i+1){
+    //     if (i==3){
+    //         continue;
+    //     }
+    //     cout<<i<<" ";
+    // }
+    
+    for (i=7; i<=70; i=i+7){
+        cout<<i<<" ";
     }
 }
+
