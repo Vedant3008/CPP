8cad4c8aaf34055ac159e0a795840227a4e9996e
Vedant3008 <shahh7107@gmail.com>
2024-05-30 17:05:26 +0530

    new question

--- While.cpp
+++ While.cpp
@@ -2,10 +2,16 @@
 using namespace std;
 
 int main(){
-    int i=1;
-    while(i<=5){
-        cout<<"Shah"<<endl;
-        i=i+1;
+    // int i=1;
+    // while(i<=5){
+    //     cout<<"Shah"<<endl;
+    //     i=i+1;
+    // }
+
+    char ch='a';
+    while(ch<='z'){
+        cout<<ch<<endl;
+        ch=ch+1;
     }
 
     return 0;
