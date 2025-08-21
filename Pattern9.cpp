af6a7629d42925508660c1f6bda18699bfb45a8f
Vedant3008 <shahh7107@gmail.com>
2025-08-21 14:30:23 -0400

    Patterns

---
+++ Pattern9.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cout<<"Enter Rows: ";
+    cin>>n;
+    for(int i=0; i<=n; i++){
+        for(int j=0; j<=2*n-1; j++){
+            if(j>=n-(i-1) && j<=n+(i-1)){
+                cout<<"*";
+            }
+            else{
+                cout<<" ";
+            }
+        }
+        cout<<endl;
+    }
+}
